8fd39afb9afafce8e075ac138fab2f6f245a67a0
irvintingsieze <irvintingg@gmail.com>
2021-08-16 15:29:10 +0800

    forgot to add annotation

--- encryption/src/main/java/com/sse/file/encryption/controller/AES_Controller.java
+++ encryption/src/main/java/com/sse/file/encryption/controller/AES_Controller.java
@@ -5,13 +5,14 @@ import com.sse.file.encryption.entity.UserLogin;
 import com.sse.file.encryption.service.AES_Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class AES_Controller {
     @PostMapping("/aes")
     public AES_Encrypt aes(@RequestBody UserLogin login){
         System.out.println(login.getPassword());
         System.out.println(login.getUsername());
         return AES_Service.main_aes(login.getUsername(),login.getPassword());
-        //return "Greetings from Spring Boot!";
     }
 }
